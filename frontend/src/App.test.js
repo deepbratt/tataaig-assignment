8f65061077772cc02e7fe5df7f364d904072f33c
Deep Bratt Mondal <deepbratt@gmail.com>
2020-07-27 10:30:49 +0530

    unitestadded

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { Provider } from "react-redux";
+import renderer from "react-test-renderer";
+import { PersistGate } from "redux-persist/integration/react";
+import App from "./App";
+import persist from "./store";
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useParams: jest
+    .fn()
+    .mockReturnValue({ environment: "dev", service: "fakeService" }),
+}));
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useSelector: jest.fn(),
+}));
+const persistStore = persist();
+test("App component snapshot", () => {
+  const component = renderer.create(
+    <Provider store={persistStore.store}>
+      <PersistGate persistor={persistStore.persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  );
+  expect(component.toJSON()).toMatchSnapshot();
 });
--- frontend/src/components/Forms/loginForm.js
+++ frontend/src/components/Forms/loginForm.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState } from "react"; 
 import { Alert, Button, Card, Container, Form, Spinner } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import allActions from "../../actions";
